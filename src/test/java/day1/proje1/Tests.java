d557b1298594af5155dacb1993d5bb1765c7bd90
YASİN AROĞLAN <yasinargln264@gmail.com>
2022-03-28 17:05:48 +0300

    Initial commit

---
+++ src/test/java/day1/proje1/Tests.java
@@ -0,0 +1,44 @@
+package day1.proje1;
+
+import day1.utils.Driver;
+import day1.utils.ParentMethots;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Tests extends ParentMethots{
+
+    String url = "https://www.saucedemo.com/";
+    By userName = By.id("user-name");
+    By password = By.id("password");
+    By login = By.id("login-button");
+    By addToCart = By.id("add-to-cart-sauce-labs-backpack");
+    By sepet = By.cssSelector("a[class='shopping_cart_link']");
+    By urunName = By.linkText("Sauce Labs Backpack");
+
+    WebDriver driver;
+
+    @BeforeTest
+            public void beforeTest(){
+        driver = Driver.getDriver();
+    }
+    @Test
+    public void test(){
+        openSite(url);
+    }
+    @Test(priority = 1)
+    public void login(){
+        sendKeysTo(userName,"standard_user");
+        sendKeysTo(password,"secret_sauce");
+        clickTo(login);
+    }
+    @Test(priority = 2)
+    public void addToCart(){
+        clickTo(addToCart);
+        clickTo(sepet);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(urunName));
+        }
+}
---
+++ src/test/java/day1/utils/ParentMethots.java
@@ -0,0 +1,42 @@
+package day1.utils;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class ParentMethots {
+
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    public ParentMethots(){
+        driver = Driver.getDriver();
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+    public void openSite(String url){
+        driver.get(url);
+    }
+    public void clickTo(By locator){
+        wait.until(ExpectedConditions.elementToBeClickable(locator)).click();
+    }
+    public void sendKeysTo(By locator,String text){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator)).sendKeys(text);
+    }
+    public void sendKeysTo(By locator,String text, boolean clear){
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+        if(clear){
+            element.clear();
+            element.sendKeys(text);
+        }
+    }
+    public void quit() throws InterruptedException {
+        Thread.sleep(2000);
+        Driver.quit();
+    }
+
+}
