ae3c5901b5171dee5ff4c2385550c3ffb0c17b46
YASİN AROĞLAN <yasinargln264@gmail.com>
2022-03-31 10:20:24 +0300

    31.03.2022

---
+++ src/test/java/day2/proje3/LocatorsClass.java
@@ -0,0 +1,9 @@
+package day2.proje3;
+
+import org.openqa.selenium.By;
+
+public interface LocatorsClass {
+
+    String url ="https://www.saucedemo.com/";
+    By users = By.cssSelector("div[id='login_credentials']");
+}
